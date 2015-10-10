f25c28320c43d5cc5472745444b062039cdffed6
Antares Chen <antares21chen@gmail.com>
2015-10-10 03:17:52 -0700

    Added utils skeleton.

---
+++ js/utils.js
@@ -0,0 +1,27 @@
+/** Utilities file for the Midpoint Application
+ *  @author Antares Chen
+ *  @author Oghenochuko Eyemaro
+ *  @author Connor Killion
+ *  @author Patrick Lee
+ */
+
+
+/** Returns a list of popular places near the location based on tge category. */
+function get_places_of_interest(location, category) {
+
+}
+
+/** Returns the midpoint of two locations. Assert that location has lat, lng. */
+function get_midpoint(location1, location2) {
+
+}
+
+/** Returns a map centered on the midpoint of start and end. */
+function center_map(map, start, end) {
+
+}
+
+/** Returns a map with the route drawn from start to end. */
+function route_map(map, start, end) {
+
+}
