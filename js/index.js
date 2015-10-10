0ef15cb0f71801512b785e357f184e3d3ee0d1de
Ckskate <ckillion@berkeley.edu>
2015-10-10 16:42:50 -0700

    Beginning Merge of Front and Back Ends

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
-$(document).ready(function() {
-  // $(document).keypress(function (e) {
-  //   var key = e.which;
-  //   if(key == 13) {
-  //     $('#submit').click();
-  //     return false;
-  //   }
+// Initialize the platform object:
+var platform = new H.service.Platform({
+  'app_id': 'rIiShqffvZj8EyDeOq15',
+  'app_code': 'Wnf__6rH5EoRKTr8WlHI7w'
+});
 
-  // $('#submit').click(function() {
-  //    $("body").css({"transform": "translate3d(0, 1000px, 0)"});
-  // });
+var maptypes = platform.createDefaultLayers();
+
+$(document).ready(function() {
+  var map = new H.Map(
+    document.getElementById('mapContainer'),
+    maptypes.normal.map,
+    {
+      zoom: 10,
+      center: { lng: 13.4, lat: 52.51 }
+  });
+
+  $(document).keypress(function (e) {
+    var key = e.which;
+    if(key == 13) {
+      $('#navInfo').submit();
+      return false;
+    }
+  });
+
+  $('#submit').click(function() {
+     $("body").css({"transform": "translate3d(0, 1000px, 0)"});
+  });
 
   $("#navInfo").submit(function(e) {
     e.preventDefault();
-    $("body").css({"transform": "translate3d(0, -75vh, 0)", "-webkit-transition": "transform 2s"});
     var address1 = $("#add1").val();
     var address2 = $("#add2").val();
     var type1 = $("#type1").val();
-    console.log("hello world");
+    if (address1 == "" || address2 == "") {
+      location.reload();
+      alert("You must enter two valid addresses!");
+      throw new Error;
+    }
+    $("body").css({"transform": "translate3d(0, -75vh, 0)", "-webkit-transition": "transform 1.5s", "-moz-transition": "transform 1.5s", "transition": "transform 1.5s"});
+    // center_map(H, map, address1, address2);
     console.log(address1, address2, type1);
   });
 });
